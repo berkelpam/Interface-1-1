6058e8c28ff77c98f7e9ac5c5c273d687854faf4
Paul Java <>
2017-12-06 14:54:47 +0100

    added saved and read list.

--- src/com/pvb/Player.java
+++ src/com/pvb/Player.java
@@ -1,5 +1,6 @@
 package com.pvb;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -73,12 +74,25 @@ public class Player implements ISaveable{
 
     @Override
     public List<String> write() {
-
+        List<String> values= new ArrayList<String>();
+        values.add(1,this.name);
+        values.add(2,this.classType);
+        values.add(3,""+ this.hitPoints);
+        values.add(4,""+this.strengh);
+        values.add(5,this.weapon);
         return null;
     }
 
     @Override
     public void read(List<String> savedValues) {
+        if (savedValues!=null && savedValues.size()>0){
+            this.name=savedValues.get(1);
+            this.classType=savedValues.get(2);
+            this.hitPoints= Integer.parseInt(savedValues.get(3));
+            this.strengh= Integer.parseInt(savedValues.get(4));
+            this.weapon=savedValues.get(5);
+
+        }
 
     }
 }
